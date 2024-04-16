11962c47cab4caa0dfa2f1f71876a24b87ad9f2f
Sam Stuewe <samuel.stuewe@gmail.com>
2024-04-16 14:40:03 -0500

    docs: warn callers that we ignore their arguments
    Signed-off-by: Sam Stuewe <samuel.stuewe@gmail.com>

--- src/somefile.c
+++ src/somefile.c
@@ -2,6 +2,8 @@
 
 /// \brief The Entry Point
 ///
+/// \warning Both parameters are ignored. We know what we're doing.
+///
 /// \param argc the number of arguments received
 /// \param argv the array of strings containing the passed arguments
 /// \returns `EXIT_SUCCESS`; we're always successful
