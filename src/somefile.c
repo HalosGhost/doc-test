416d4896a59f2ee2b6f49a2c4cdcc77d1cebdf08
Sam Stuewe <samuel.stuewe@gmail.com>
2024-04-16 14:36:43 -0500

    init: test external doxygen deployment
    Signed-off-by: Sam Stuewe <samuel.stuewe@gmail.com>

---
+++ src/somefile.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/// \brief The Entry Point
+///
+/// \param argc the number of arguments received
+/// \param argv the array of strings containing the passed arguments
+/// \returns `EXIT_SUCCESS`; we're always successful
+signed
+main (signed argc, char * argv []) {
+
+    // silence compiler warnings
+    (void )argc;
+    (void )argv;
+    return EXIT_SUCCESS;
+}
